5bd2e28e9fa6baf1731e1c36042786098b6bc90f
Mituleon00 <mituleon15@gmail.com>
2025-01-26 10:51:13 -0800

    hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,26 @@
+//c++ function to calculate volume of a cylinder
+#include <iostream>
+#include <cmath> // for M_PI
+using namespace std;
 
+    double calculatecylindervolume(double radius, double height) {
+        double volume = M_PI * radius * radius * height;
+        return volume;
+    }
+    
+int main() {
+    double radius, height;
+    
+    //prompt user to enter radius
+    cout <<"Enter the radius: ";
+    cin >> radius;
+    
+    //prompt user to enter height
+    cout <<"Enter the height: ";
+    cin >> height;
+    
+    //calculate and display the volume
+    double volume = calculatecylindervolume(radius, height);
+    cout <<"The volume of cylinder is: " << volume << endl;
+    return 0;
+}
